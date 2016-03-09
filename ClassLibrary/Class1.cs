91892d110865691f01e817a0665c6dfe3fdf3d29
david <biki3507@gmail.com>
2016-03-09 21:35:51 +0800

    int version

---
+++ ClassLibrary/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Class1
+    {
+        public List<int> GetSample1(int groupNum)
+        {
+            List<int> data = new List<int>();
+
+            switch (groupNum)
+            {
+                case 3:
+                    data.Add(6);
+                    data.Add(15);
+                    data.Add(24);
+                    data.Add(21);
+                    break;
+                case 4:
+                    data.Add(50);
+                    data.Add(66);
+                    data.Add(60);
+                    break;
+            }
+
+            return data;
+        }
+
+
+    }
+}
---
+++ ClassLibraryTests/Class1Tests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExpectedObjects;
+namespace ClassLibrary.Tests
+{
+    [TestClass()]
+    public class Class1Tests
+    {
+        [TestMethod()]
+        public void GetSample1Test_GroupNum_3_Result_must_be_6_15_24_21()
+        {
+            //arrange
+            var target = new Class1();
+            var expected = new List<int>() {6,24,15,21 };
+            //act
+            var actual = target.GetSample1(3);
+            //assert
+
+            CollectionAssert.AreEquivalent(expected, actual);
+        }
+        [TestMethod()]
+        public void GetSample1Test_GroupNum_4_Result_must_be_50_66_60()
+        {
+            //arrange
+            var target = new Class1();
+            var expected = new List<int>() { 50,66,60 };
+            //act
+            var actual = target.GetSample1(4);
+            //assert
+
+            CollectionAssert.AreEquivalent(expected, actual);
+        }
+    }
+}
